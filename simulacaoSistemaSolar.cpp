ac2e21c31e1e12ea76ab7a6562dc4b720020b860
Breno Bertone <bertonebreno@gmail.com>
2020-03-02 12:06:09 -0300

    feat: 🎸 Added a collision system

--- simulacaoSistemaSolar.cpp
+++ simulacaoSistemaSolar.cpp
@@ -3,10 +3,13 @@
 #include <math.h>
 #include <opencv2/opencv.hpp>
 
-//compile with g++ -o simulacao simulacaoSistemaSolar.cpp `pkg-config opencv --cflags --libs`
+#define BODYNUMBER 12
 
 using namespace cv;
 using namespace std;
+
+//compile with g++ -o simulacao simulacaoSistemaSolar.cpp `pkg-config opencv --cflags --libs`
+
 
 double G = 1;
 double gForce(double mass1, double mass2, double distance){
@@ -14,32 +17,26 @@ double gForce(double mass1, double mass2, double distance){
 }
 
 int main(){
-	double position[5][2];
-	double velocity[5][2];
-	long mass[5];
+	double position[BODYNUMBER][2];
+	double velocity[BODYNUMBER][2];
+	long mass[BODYNUMBER];
+	double radius[BODYNUMBER];
 
 
-	double timeDiff = 0.00001;
+	double timeDiff = 0.000001;
 
-	mass[0] = 42875000e+4;
-	position[0][0] = 400;
-	position[0][1] = 400;
-	velocity[0][0] = 0;
-	velocity[0][1] = 0;
-
-
-	mass[1] = 1e+5;
-	position[1][0] = 50;
-	position[1][1] = 400;
-	velocity[1][0] = 0;
-	velocity[1][1] = 35000;
-
-
-	mass[2] = 1;
-	position[2][0] = 50;
-	position[2][1] = 410;
-	velocity[2][0] = 100;
-	velocity[2][1] = 35000;
+	long defaultMass = 3000000000;
+	for(int i = 0; i < BODYNUMBER; i += 1){
+		mass[i] = defaultMass*(i+1);
+		radius[i] = 10*mass[i]/(defaultMass*(BODYNUMBER+1));
+		if(radius[i]<1){
+			radius[i] = 1;
+		}
+		position[i][0] = 60*(i+1);
+		position[i][1] = 30*(i+1);
+		velocity[i][0] = 0;
+		velocity[i][1] = 0;
+	}
 
 
 	String windowName = "Window with Blank Image"; //Name of the window
@@ -48,41 +45,42 @@ int main(){
 	int x = 1;
 	while(x){
 
-		for(int i = 0; i<3; i+=1){
+		for(int body1 = 0; body1<BODYNUMBER; body1+=1){
 			double oldPosition[2];
-			oldPosition[0] = position[i][0];
-			oldPosition[1] = position[i][1];
-			position[i][0] += velocity[i][0]*timeDiff;
-			position[i][1] += velocity[i][1]*timeDiff;				
-			for(int j = i; j<3; j+=1){
-				if(i!=j){
-					double deltaX = (oldPosition[0]-position[j][0]);
-					double deltaY = (oldPosition[1]-position[j][1]);
+			oldPosition[0] = position[body1][0];
+			oldPosition[1] = position[body1][1];
+			position[body1][0] += velocity[body1][0]*timeDiff;
+			position[body1][1] += velocity[body1][1]*timeDiff;				
+			for(int body2 = body1; body2<BODYNUMBER; body2+=1){
+				if(body1!=body2){
+					double deltaX = (oldPosition[0]-position[body2][0]);
+					double deltaY = (oldPosition[1]-position[body2][1]);
 					double distance = sqrt((deltaX*deltaX)+(deltaY*deltaY));
-					// printf("OldP: %lf %lf\n", oldPosition[0], oldPosition[1]);
-					// printf("dX:%lf dY:%lf S:%lf\n", deltaX, deltaY, distance);
+					double sinTheta = deltaY/distance;
+					double cosTheta = deltaX/distance;
 
-					if(distance!=0){
-						double sinTheta = deltaY/distance;
-						double cosTheta = deltaX/distance;
-						// printf("s:%lf c:%lf\n", sinTheta, cosTheta);
+					if(distance < radius[body1] + radius[body2]){
+						velocity[body1][0] = (mass[body1]*velocity[body1][0]-mass[body2]*velocity[body1][0]+2*mass[body2]*velocity[body2][0])/(mass[body1]+mass[body2]);
+						velocity[body1][1] = (mass[body1]*velocity[body1][1]-mass[body2]*velocity[body1][1]+2*mass[body2]*velocity[body2][1])/(mass[body1]+mass[body2]);
+						velocity[body2][0] = -(mass[body2]*velocity[body2][0]-mass[body1]*velocity[body2][0]+2*mass[body1]*velocity[body1][0])/(mass[body1]+mass[body2]);
+						velocity[body2][1] = -(mass[body2]*velocity[body2][1]-mass[body1]*velocity[body2][1]+2*mass[body1]*velocity[body1][1])/(mass[body1]+mass[body2]);
+					}
+					else if(distance!=0){
+						double force = gForce(mass[body1], mass[body2], distance);
 
-						double force = gForce(mass[i], mass[j], distance);
-						// printf("f:%lf\n", force);
-
-						velocity[i][0] -= (force/mass[i])*cosTheta*timeDiff;
-						velocity[i][1] -= (force/mass[i])*sinTheta*timeDiff;
-						velocity[j][0] += (force/mass[j])*cosTheta*timeDiff;
-						velocity[j][1] += (force/mass[j])*sinTheta*timeDiff;
+						velocity[body1][0] -= (force/mass[body1])*cosTheta*timeDiff;
+						velocity[body1][1] -= (force/mass[body1])*sinTheta*timeDiff;
+						velocity[body2][0] += (force/mass[body2])*cosTheta*timeDiff;
+						velocity[body2][1] += (force/mass[body2])*sinTheta*timeDiff;
 					}
 				}
 			}
 		}
 
-    Mat image(800, 800, CV_8UC3, Scalar(0, 0, 0));
-	circle(image, Point(position[0][0], position[0][1]), 15, Scalar(255, 255, 255),CV_FILLED, 8,0);
-	circle(image, Point(position[1][0], position[1][1]), 7, Scalar(255, 255, 255),CV_FILLED, 8,0);
-	circle(image, Point(position[2][0], position[2][1]), 2, Scalar(255, 255, 255),CV_FILLED, 8,0);
+    Mat image(700, 1200, CV_8UC3, Scalar(0, 0, 0));
+	for(int i = 0; i<BODYNUMBER; i+=1	){
+		circle(image, Point((int) position[i][0], (int) position[i][1]), radius[i], Scalar(255, 255, 255),CV_FILLED, 8,0);
+	}
 	imshow(windowName, image); // Show our image inside the created window.
 	waitKey(1);
 	}
