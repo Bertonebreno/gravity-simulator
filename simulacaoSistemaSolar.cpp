4f7d42be3bf174f3819d3bbd8135e240276d6c09
Breno Bertone <bertonebreno@gmail.com>
2020-03-03 10:41:57 -0300

    feat: 🎸 Bodies are now automatic added;removed bugged collision

--- simulacaoSistemaSolar.cpp
+++ simulacaoSistemaSolar.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <opencv2/opencv.hpp>
 
-#define BODYNUMBER 12
+#define BODYNUMBER 20
 
 using namespace cv;
 using namespace std;
@@ -21,19 +21,21 @@ int main(){
 	double velocity[BODYNUMBER][2];
 	long mass[BODYNUMBER];
 	double radius[BODYNUMBER];
+	int color[BODYNUMBER][3];
 
+	double timeDiff = 0.00001;
 
-	double timeDiff = 0.000001;
-
-	long defaultMass = 3000000000;
+	long maxMass = 2147483647;
+	long minMass = 100;
 	for(int i = 0; i < BODYNUMBER; i += 1){
-		mass[i] = defaultMass*(i+1);
-		radius[i] = 10*mass[i]/(defaultMass*(BODYNUMBER+1));
-		if(radius[i]<1){
-			radius[i] = 1;
-		}
-		position[i][0] = 60*(i+1);
-		position[i][1] = 30*(i+1);
+		color[i][0] = rand() % 255;
+		color[i][1] = rand() % 255;
+		color[i][2] = rand() % 255;
+		mass[i] = rand() % maxMass + minMass;
+		printf("%ld", mass[i]);
+		radius[i] = 2*(mass[i]*14/2147483547 + 2147482147/2147483547);
+		position[i][0] = rand() % 1000+ 100;
+		position[i][1] = rand() % 500 + 100;
 		velocity[i][0] = 0;
 		velocity[i][1] = 0;
 	}
@@ -46,11 +48,13 @@ int main(){
 	while(x){
 
 		for(int body1 = 0; body1<BODYNUMBER; body1+=1){
+
 			double oldPosition[2];
 			oldPosition[0] = position[body1][0];
 			oldPosition[1] = position[body1][1];
 			position[body1][0] += velocity[body1][0]*timeDiff;
-			position[body1][1] += velocity[body1][1]*timeDiff;				
+			position[body1][1] += velocity[body1][1]*timeDiff;	
+
 			for(int body2 = body1; body2<BODYNUMBER; body2+=1){
 				if(body1!=body2){
 					double deltaX = (oldPosition[0]-position[body2][0]);
@@ -60,10 +64,7 @@ int main(){
 					double cosTheta = deltaX/distance;
 
 					if(distance < radius[body1] + radius[body2]){
-						velocity[body1][0] = (mass[body1]*velocity[body1][0]-mass[body2]*velocity[body1][0]+2*mass[body2]*velocity[body2][0])/(mass[body1]+mass[body2]);
-						velocity[body1][1] = (mass[body1]*velocity[body1][1]-mass[body2]*velocity[body1][1]+2*mass[body2]*velocity[body2][1])/(mass[body1]+mass[body2]);
-						velocity[body2][0] = -(mass[body2]*velocity[body2][0]-mass[body1]*velocity[body2][0]+2*mass[body1]*velocity[body1][0])/(mass[body1]+mass[body2]);
-						velocity[body2][1] = -(mass[body2]*velocity[body2][1]-mass[body1]*velocity[body2][1]+2*mass[body1]*velocity[body1][1])/(mass[body1]+mass[body2]);
+
 					}
 					else if(distance!=0){
 						double force = gForce(mass[body1], mass[body2], distance);
@@ -79,7 +80,7 @@ int main(){
 
     Mat image(700, 1200, CV_8UC3, Scalar(0, 0, 0));
 	for(int i = 0; i<BODYNUMBER; i+=1	){
-		circle(image, Point((int) position[i][0], (int) position[i][1]), radius[i], Scalar(255, 255, 255),CV_FILLED, 8,0);
+		circle(image, Point((int) position[i][0], (int) position[i][1]), radius[i], Scalar(color[i][0], color[i][1], color[i][2]),CV_FILLED, 8,0);
 	}
 	imshow(windowName, image); // Show our image inside the created window.
 	waitKey(1);
